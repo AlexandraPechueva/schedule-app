634a1d65022e0d4992c420bcf83bfb435a155946
Alexandra <AlexandraSchetchikova@yandex.ru>
2020-07-31 12:05:22 +0300

    Add html validation and sorting when time has changed

--- src/app/components/day-schedule/day-schedule.component.ts
+++ src/app/components/day-schedule/day-schedule.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnChanges, ViewChild, ElementRef } from '@angular/core';
 import { LocalStorageService } from 'src/app/services/local-storage.service';
+import { NgForm } from '@angular/forms';
 
 interface Record {
   time: string;
@@ -17,19 +18,35 @@ export class DayScheduleComponent implements OnChanges {
   ) { }
 
   @Input() activatedDay = '';
+  @ViewChild('todoForm') private readonly _form: NgForm;
   @ViewChild('time') timeInput: ElementRef;
   @ViewChild('task') taskInput: ElementRef;
 
   weekRecords = this._localStorage.get('weekRecords') || {};
   dayRecords: Record[] = [];
 
-  records: Record[] = [];
+  private _newTimeValue = '';
 
   ngOnChanges() {
     this._setDayRecords(this.weekRecords, this.activatedDay);
   }
 
+  customTrackBy(index: number): number {
+    return index;
+  }
+
+  inputFocusOut(i: number) {
+    this.weekRecords[this.activatedDay][i].time = this._newTimeValue;
+    this._setDayRecords(this.weekRecords, this.activatedDay);
+  }
+
+  timeModelChangeFn(value) {
+    this._newTimeValue = value;
+  }
+
   addTodo(time: string, task: string) {
+    if (this._form.invalid) return;
+
     if (!this.weekRecords[this.activatedDay]) {
       this.weekRecords[this.activatedDay] = [];
     }
